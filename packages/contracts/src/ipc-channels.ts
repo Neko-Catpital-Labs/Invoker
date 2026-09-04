3c81d732c0748ddd6f17db1ac140bbcec6e3c4b4
Edbert Chan <edbert.chan@yahoo.com>
2026-09-03 19:13:34 -0700

    Add invoker:tick-worker IPC contract and tsconfig compatibility
    Change-Id: Idb92c693c8589519ebca5847ab9e992722a714d4

--- packages/contracts/src/ipc-channels.ts
+++ packages/contracts/src/ipc-channels.ts
@@ -1309,6 +1309,10 @@ export const IpcChannels = {
     request: [kind: string];
     response: WorkerStatusEntry;
   },
+  'invoker:tick-worker': {} as {
+    request: [kind: string];
+    response: WorkerStatusEntry;
+  },
   'invoker:get-action-graph': {} as {
     request: [];
     response: ActionGraphResponse;
