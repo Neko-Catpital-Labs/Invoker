0deb92927a288bceb54149b270896514e3835f41
Edbert Chan <edbert.chan@yahoo.com>
2026-09-15 00:19:25 -0700

    Add periodic workflow cleanup worker
    Change-Id: I6c17e3fac7f5c0cbce9f1317db05da3a7a00135f

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -407,6 +407,10 @@ function buildRegisteredOwnerWorkerDeps(
 
   return {
     store,
+    deleteWorkflow: (workflowId) => orchestrator.deleteWorkflow(workflowId),
+    workflowCleanup: {
+      listWorkflows: () => persistence.listWorkflows(),
+    },
     submitter: {
       submit: submitRegisteredOwnerWorkerMutation,
     },
